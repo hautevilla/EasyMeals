cc90e10a7912914417450d167f719c0fca463903
Raj Vansia <vansia112@gmail.com>
2017-01-29 08:47:51 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDXFXVYCirwgTnUIW98wMF5s52bI6AahDo",
-    authDomain: "foodtracker-4c72f.firebaseapp.com",
-    databaseURL: "https://foodtracker-4c72f.firebaseio.com",
-    storageBucket: "foodtracker-4c72f.appspot.com",
-    messagingSenderId: "23826452842"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
  const firebaseApp = firebase.initializeApp(firebaseConfig);
  const fireApp = firebaseApp.database();
